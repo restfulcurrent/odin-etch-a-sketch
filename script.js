5e95cf32e075127dcfd4ca924b9a49a8b79680d7
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-10 15:17:51 -0500

    Add logic to create a 16x16 grid of squares

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, Etch-a-Sketch!");
+createGrid();
+
+function createGrid() {
+    const SIDE_LENGTH = 16;
+    
+    for (let i = 0; i < SIDE_LENGTH; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        row.setAttribute("id", `r-${i}`);
+    
+        for (let j = 0; j < SIDE_LENGTH; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.setAttribute("class", "grid-square");
+            gridSquare.setAttribute("id", `gs-${i}-${j}`);
+    
+            row.appendChild(gridSquare);
+        }
+    
+        const container = document.querySelector(".container");
+        container.appendChild(row);
+    }
+}
