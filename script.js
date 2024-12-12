b3f1834fab9a29269320a2a2cbbf46e27a3b24ba
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-12 15:02:52 -0500

    Add lhs, grid, rhs divs as children of content wrapper

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const gridContainer = document.querySelector(".grid-container");
+const contentWrapper = document.querySelector(".content-wrapper");
+
+const lhs = document.createElement("div");
+lhs.classList.add("lhs");
+contentWrapper.append(lhs);
+
 const INITIAL_SIDELENGTH = 16;
-initializeGrid(gridContainer, INITIAL_SIDELENGTH);
-enablePainting(gridContainer);
+const grid = createGrid(INITIAL_SIDELENGTH);
+contentWrapper.append(grid);
 
-const gridDensityBtn = document.createElement("button");
-initializeGridDensityBtn(gridDensityBtn);
+const rhs = document.createElement("div");
+rhs.classList.add("rhs");
+contentWrapper.append(rhs);
 
-// Creates and displays an n x n grid of squares where n is the side length in terms of squares
-// Input: grid container div, side length (n)
-function initializeGrid(gridContainer, sideLength) {    
+// enablePainting(grid);
+
+// const gridDensityBtn = document.createElement("button");
+// initializeGridDensityBtn(gridDensityBtn);
+
+// Create an n x n grid of squares where n is the side length in terms of squares
+// Input: side length (in units of grid squares)
+// Returns a reference to the grid (node element)
+function createGrid(sideLength) { 
+    const grid = document.createElement("div");
+    grid.classList.add("grid");   
+
     for (let i = 0; i < sideLength; i++) {
         // Create row container
         const row = document.createElement("div");
@@ -23,67 +38,69 @@ function initializeGrid(gridContainer, sideLength) {
     
             row.appendChild(gridSquare);
         }
-        // Display row (append to gridcontainer)
-        gridContainer.appendChild(row);
+        // Display row (append to grid)
+        grid.appendChild(row);
     }
+
+    return grid;
 }
 
 // Paintsgrid squares when they are hovered over
-function enablePainting(gridContainer) {
-    gridContainer.addEventListener("mouseover", logMouseOver);
-    gridContainer.addEventListener("mouseout", logMouseOut);
+// function enablePainting(grid) {
+//     grid.addEventListener("mouseover", logMouseOver);
+//     grid.addEventListener("mouseout", logMouseOut);
     
-    function logMouseOver(event) {
-        // Get the hovered grid square
-        const target = event.target;
+//     function logMouseOver(event) {
+//         // Get the hovered grid square
+//         const target = event.target;
 
-        // If target is not a grid square, do nothing
-        if (!target.classList.contains("grid-square")) return;
+//         // If target is not a grid square, do nothing
+//         if (!target.classList.contains("grid-square")) return;
 
-        console.log(`over -> ${target.id}`);
+//         console.log(`over -> ${target.id}`);
     
-        // Paint the grid square
-        target.classList.add("hovered");
-    }
+//         // Paint the grid square
+//         target.classList.add("hovered");
+//     }
     
-    function logMouseOut(event) {
-        // Get the hovered grid square
-        const target = event.target;
-        console.log(`out <- ${target.id}`);
-    }
-}
+//     function logMouseOut(event) {
+//         // Get the hovered grid square
+//         const target = event.target;
+//         console.log(`out <- ${target.id}`);
+//     }
+// }
 
 // Create and display button to change grid density
-function initializeGridDensityBtn(gridDensityBtn) {
-    gridDensityBtn.setAttribute("type", "button");
-    gridDensityBtn.textContent = "Change grid density";
-    gridDensityBtn.addEventListener("click", initiateGridDensityChange);
+// function initializeGridDensityBtn(gridDensityBtn) {
+//     gridDensityBtn.setAttribute("type", "button");
+//     gridDensityBtn.textContent = "Change grid density";
+//     gridDensityBtn.addEventListener("click", initiateGridDensityChange);
     
-    gridContainer.before(gridDensityBtn);
+//     grid.before(gridDensityBtn);
 
-    function initiateGridDensityChange() {
-        const MIN_SIDE_LENGTH = 2;
-        const MAX_SIDE_LENGTH = 100;
+//     function initiateGridDensityChange() {
+//         const MIN_SIDE_LENGTH = 2;
+//         const MAX_SIDE_LENGTH = 100;
         
-        // Get new side length from user (in units of grid squares)
-        const sideLength = parseInt(
-            prompt(`Enter a side length (grid squares) between ${MIN_SIDE_LENGTH} and ${MAX_SIDE_LENGTH}`, "")
-        );
+//         // Get new side length from user (in units of grid squares)
+//         const sideLength = parseInt(
+//             prompt(`Enter a side length (grid squares) between ${MIN_SIDE_LENGTH} and ${MAX_SIDE_LENGTH}`, "")
+//         );
         
-        // Do nothing if user input is invalid
-        if (isNaN(sideLength) || 
-        sideLength < MIN_SIDE_LENGTH || 
-        sideLength > MAX_SIDE_LENGTH) {
-            console.log(`Input: ${sideLength}, invalid input.`);
-            return;
-        }
+//         // Do nothing if user input is invalid
+//         if (isNaN(sideLength) || 
+//         sideLength < MIN_SIDE_LENGTH || 
+//         sideLength > MAX_SIDE_LENGTH) {
+//             console.log(`Input: ${sideLength}, invalid input.`);
+//             return;
+//         }
         
-        // Remove the existing grid
-        while (gridContainer.firstChild) {
-            gridContainer.firstChild.remove();
-        }
+//         // Remove the existing grid
+//         while (grid.firstChild) {
+//             grid.firstChild.remove();
+//         }
         
-        // Create a new grid with user-specified side length
-        initializeGrid(gridContainer, sideLength);
-    }
-}
+//         // Create a new grid with user-specified side length
+//         createGrid(grid, sideLength);
+//     }
+// }
